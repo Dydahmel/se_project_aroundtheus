9f2bf2f77806e039caeee96c66cbbbb275f423a4
Anton Dutchak <dydahmel@gmail.com>
2023-01-11 12:21:04 -0500

    sprint 4.1 update

---
+++ scripts/index.js
@@ -0,0 +1,32 @@
+const initialCards[object1, object2, object3, object4, object5, object6] 
+
+object1{
+    name: 'Yosemite Valley',
+    link: 'https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg'
+}
+
+object2{
+    name: 'Lake Louise',
+    link: 'https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg'
+}
+
+object3{
+    name: 'Bald Mountains',
+    link: 'https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg'
+}
+
+object4{
+    name: 'Latemar',
+    link: 'https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg'
+}
+
+object5{
+    name: 'Vanoise National Park',
+    link: 'https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg'
+}
+
+object6{
+    name: 'Lago di Braies',
+    link: 'https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg'
+}
+
