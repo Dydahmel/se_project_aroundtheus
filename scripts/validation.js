bfb657083d7042fa98741add904a91af50389978
Anton Dutchak <dydahmel@gmail.com>
2023-02-10 13:13:22 -0500

    BEM

--- scripts/validation.js
+++ scripts/validation.js
@@ -4,7 +4,7 @@ const config = {
     inputSelector: ".modal__input",
     submitButtonSelector: ".modal__save-button",
     inactiveButtonClass: "modal__save-button-disabled",
-    inputErrorClass: ".modal__input_type_error",
+    inputErrorClass: ".modal__input_type-error",
     errorClass: ".modal__error_visible"
   }; 
 
