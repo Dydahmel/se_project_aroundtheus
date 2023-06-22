a06c8b30b4ebb91e94f0322b2c01c67dadfff9c1
Anton Dutchak <106831892+Dydahmel@users.noreply.github.com>
2023-06-22 12:37:00 -0400

    update

--- src/components/PopupDelete.js
+++ src/components/PopupDelete.js
@@ -2,5 +2,12 @@ import Popup from "./Popup";
 export default class PoppurDelete extends Popup{
     constructor(popupSelector){
         super({popupSelector})
+        this._card = this._popupElement.querySelector(".card")
     }
+
+    setEventListeners(){
+        super.setEventListeners()
+    }
+
+    
 };
